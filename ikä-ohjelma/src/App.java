96add491b818cf1b88bffd3fc459f04ae6f99c5f
EetuPaavilainen <eetu.paavilainen@student.hamk.fi>
2024-10-10 11:30:44 +0300

    ikä ohjelma
    perus ikä ohjelma luotu

---
+++ ikä-ohjelma/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    int ika = 45;
+        // tulostusehdot
+    if (ika > 0 && ika < 18)
+    {
+        System.out.println("Olet alaikäinen");
+    }
+    else if (ika >= 65)
+    {
+        System.out.println("Olet eläkeläinen");
+    }
+    else 
+    {
+        System.out.println("Olet aikuinen");
+    }
+
+}
+}
